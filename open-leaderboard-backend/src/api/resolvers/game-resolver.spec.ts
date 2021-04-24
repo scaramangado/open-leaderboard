ae1f691dae5ce1b392a38d0acc8753417e163459
scaramangado <scaramangado@gmail.com>
2021-04-24 13:06:01 +0200

    Use jest instead of custom mocks

--- open-leaderboard-backend/src/api/resolvers/game-resolver.spec.ts
+++ open-leaderboard-backend/src/api/resolvers/game-resolver.spec.ts
@@ -1,75 +1,75 @@
-import gameRepository, { GameRepository } from "../../data/games/game-repository";
+import { GameRepository } from "../../data/games/game-repository";
 import Game from "../model/game";
 import GameResolver from "./game-resolver";
 
-describe("Game Resolver Test", () => {
+const gameRepositoryMock: GameRepository = {
+  findAllGames: jest.fn(),
+  findGameBySlug: jest.fn(),
+};
 
+const resolver = new GameResolver(gameRepositoryMock);
 
-  test("Returns full game list", () => {
+test("Returns full game list", () => {
 
-    const games: Game[] = [
-      {
-        slug: "test1",
-        name: "name1",
-        releaseDate: new Date(Date.UTC(2021, 3, 20, 0, 0, 0)),
-      },
-      {
-        slug: "test2",
-        name: "name2",
-        releaseDate: new Date(Date.UTC(1990, 3, 20, 0, 0, 0)),
-      },
-    ];
+  const games: Game[] = [
+    {
+      slug: "test1",
+      name: "name1",
+      releaseDate: new Date(Date.UTC(2021, 3, 20, 0, 0, 0)),
+    },
+    {
+      slug: "test2",
+      name: "name2",
+      releaseDate: new Date(Date.UTC(1990, 3, 20, 0, 0, 0)),
+    },
+  ];
 
-    const repoMock: GameRepository = {
-      findGameBySlug: (_slug: String) => undefined,
-      findAllGames: () => games,
-    }
+  givenRepositoryReturnsGames(games);
 
-    expect(new GameResolver(repoMock).games())
-      .toEqual(
-        [
-          {
-            name: "name1",
-            slug: "test1",
-            releaseDate: "2021-04-20T00:00:00.000Z",
-          },
-          {
-            name: "name2",
-            slug: "test2",
-            releaseDate: "1990-04-20T00:00:00.000Z",
-          },
-        ]
-      );
-
-  });
-
-  test("Returns game with slug", () => {
-
-    const repoMock: GameRepository = {
-      findGameBySlug: (slug: String) => {
-        if (slug !== "test1") {
-          return undefined;
-        }
-
-        return {
-          slug: "test1",
-          name: "name1",
-          releaseDate: new Date(Date.UTC(2021, 3, 20, 0, 0, 0)),
-        };
-      },
-      findAllGames: () => [],
-    }
-
-    expect(new GameResolver(repoMock).game({ slug: "test1" }))
-      .toEqual(
+  expect(resolver.games())
+    .toEqual(
+      [
         {
           name: "name1",
           slug: "test1",
           releaseDate: "2021-04-20T00:00:00.000Z",
-        }
-      );
-  });
+        },
+        {
+          name: "name2",
+          slug: "test2",
+          releaseDate: "1990-04-20T00:00:00.000Z",
+        },
+      ]
+    );
 
 });
 
+test("Returns game with slug", () => {
 
+  const game :Game= {
+    slug: "test1",
+    name: "name1",
+    releaseDate: new Date(Date.UTC(2021, 3, 20, 0, 0, 0)),
+  };
+
+  givenRepositoryReturnsGameForSlug(game);
+
+  expect(resolver.game({ slug: "test1" }))
+    .toEqual(
+      {
+        name: "name1",
+        slug: "test1",
+        releaseDate: "2021-04-20T00:00:00.000Z",
+      }
+    );
+});
+
+function givenRepositoryReturnsGames(games: Game[]) {
+  (<jest.Mock<Game[]>>gameRepositoryMock.findAllGames)
+    .mockImplementation(() => games);
+}
+
+function givenRepositoryReturnsGameForSlug(game: Game) {
+  (<jest.Mock<Game>>gameRepositoryMock.findGameBySlug)
+    .mockImplementation(slug => game);
+}
